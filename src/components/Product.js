b3c8cbbdb25a20d4b1ba8a78352663d41e47b7f0
purisquiri <mau.dorigo@outlook.es>
2021-01-27 14:13:33 -0500

    user orders looks better

--- src/components/Product.js
+++ src/components/Product.js
@@ -42,7 +42,7 @@ export default class Product extends Component {
             <p className="align-self-center text-blue mb-0">{title}</p>
             <h5 className="text-blue font-italic mb-0">
               <span className="mr-1">$</span>
-              {price}
+              {price.toFixed(2)}
             </h5>
           </div>
         </div>
